a022e945a9c1c6b91004e0e38222608c4c1836fb
Taesu <bytaesu@gmail.com>
2026-03-25 10:25:50 +0900

    debug casing

--- convex/auth.ts
+++ convex/auth.ts
@@ -31,7 +31,7 @@ export const createAuth = (ctx: GenericCtx<DataModel>) => {
       convex({ authConfig }),
       oAuthProxy({
         productionURL: "https://ba-oauth-proxy-convex.vercel.app",
-        currentURL: CURRENT_URL,
+        currentUrl: CURRENT_URL,
       }),
     ],
     trustedOrigins: [
